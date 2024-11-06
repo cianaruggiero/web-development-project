690a60f922f3964a966a68fa687b9a15fb981a37
cianaruggiero <cianaruggiero@gmail.com>
2024-11-06 18:45:09 -0500

    History

---
+++ form.js
@@ -0,0 +1,37 @@
+const express = require ('express');
+const nodemailer = require ('nodemailer');
+const bodyParser = require ('body-parser');
+const port = 3000;
+
+const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(express.static("Public"));
+
+app.post('/send-email', async (req, res) => {
+    const{name, email, message} = req.body;
+    const transporter = nodemailer.createTransport({
+        service:"gmail",
+        auth: {
+            user:"email@gmail.com",
+            pass:"password123"
+        }
+    });
+    const mailOptions = {
+        from:email,
+        to:"cianaruggiero@gmail.com",
+        subject:"contact form submission",
+        text:message
+    };
+    try {
+        await transporter.sendMail(mailOptions);
+        res.status(200).send("Message Sent");
+    }   catch (error) {
+        res.status(500).send("Failed to Send");
+        console.error(error);
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
